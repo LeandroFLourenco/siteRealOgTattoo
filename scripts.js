1cab0df277048df885cf39d3409d00f2c430ebca
LeandroFLourenco <leandrinlourenco@gmail.com>
2025-11-07 16:56:57 -0300

    feat: Salvando alteracoes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const items = document.querySelectorAll('.item');
+const dots = document.querySelectorAll('.dot');
