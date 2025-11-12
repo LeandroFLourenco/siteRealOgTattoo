8d3ad24afba3aae39fea122cea58e8b031915bf9
LeandroFLourenco <leandrinlourenco@gmail.com>
2025-11-12 16:55:43 -0300

    feat: realizando correcoes no projeto e deixado ok ate onde foi feito

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('arrow-left');
+const nextButton = document.getElementById('arrow-right');
 const items = document.querySelectorAll('.item');
 const dots = document.querySelectorAll('.dot');
 const numberIndicator = document.querySelector('.numbers');
 const list = document.querySelector('.list');
+
+
+let active = 0;
+const total = items.length;
+let timer;
+
+prevButton.addEventListener('click', function() {
+    alert('Previous button clicked');
+})
+
+nextButton.addEventListener('click', function() {
+    alert('Next button clicked');
+})
