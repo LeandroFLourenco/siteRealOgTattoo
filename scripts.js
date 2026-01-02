1d5523b39f2019f3fba2a1908c51d40919783083
LeandroFLourenco <leandrinlourenco@gmail.com>
2026-01-02 16:46:35 -0300

    feat: Correcoes

--- scripts.js
+++ scripts.js
@@ -18,20 +18,24 @@ function update(direction) {
     if(direction > 0) {
         active = active + 1
 
-        if(active >= total) {
+        if(active === total) {
+            active = 0;
+        }
+
     } 
     
-    else  if(direction < 0) {
+    else if(direction < 0) {
+        active = active - 1
 
+        if(active < 0) {
+            active = total - 1;
+        }
     }
 
-
-
-
-
-
-
-
+    items[active].classList.add('active');
+    dots[active].classList.add('active');
+    numberIndicator.textContent = String(active + 1).padStart(2, '0');
+}
 
 prevButton.addEventListener('click', function() {
     update(-1);
